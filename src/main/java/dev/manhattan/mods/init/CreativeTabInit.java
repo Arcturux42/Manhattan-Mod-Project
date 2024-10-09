af6219b6b9f1fe7271d878b7c4f8b2eae9be64ae
TimTeam0312 <155571149+TimTeam0312@users.noreply.github.com>
2024-10-09 22:18:39 +0200

    Bro.... I really don't want to commit so just remember there's PIZZA!!!!

--- src/main/java/dev/manhattan/mods/init/CreativeTabInit.java
+++ src/main/java/dev/manhattan/mods/init/CreativeTabInit.java
@@ -6,9 +6,12 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -31,7 +34,7 @@ public class CreativeTabInit {
                     .displayItems((displayParams, output) ->
                             MANHATTAN_TAB_ITEMS.forEach(itemLike -> output.accept(itemLike.get()))
                     )
-                    .withSearchBar()
+                    .withSearchBar(80)
                     .build()
     );
 
@@ -43,13 +46,36 @@ public class CreativeTabInit {
     @SubscribeEvent
     public static void buildContents(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey().equals(CreativeModeTabs.BUILDING_BLOCKS)) {
-            event.accept(ItemsInit.URANIUM_BLOCK_ITEM);
+            event.getEntries().putAfter(Items.BASALT.getDefaultInstance(), ItemsInit.URANIUM_BLOCK_ITEM.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
             event.accept(ItemsInit.CARBON_BLOCK_ITEM);
             event.accept(ItemsInit.LEAD_BLOCK_ITEM);
         }
 
         if(event.getTabKey().equals(CreativeModeTabs.FOOD_AND_DRINKS)) {
-            event.accept(ItemsInit.CHOCOLATE);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.PUMPKIN.getDefaultInstance(),
+                            ItemsInit.CHEESE.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.PUMPKIN.getDefaultInstance(),
+                            ItemsInit.CHOCOLATE.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.PUMPKIN.getDefaultInstance(),
+                            ItemsInit.SLICE_OF_PIZZA.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.PUMPKIN.getDefaultInstance(),
+                            ItemsInit.PIZZA.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
         }
 
         if(event.getTabKey().equals(CreativeModeTabs.NATURAL_BLOCKS)) {
@@ -57,18 +83,60 @@ public class CreativeTabInit {
             event.accept(ItemsInit.URANIUM_ORE_ITEM);
         }
         if(event.getTabKey().equals(CreativeModeTabs.COMBAT)) {
-            event.accept(ItemsInit.LEAD_SWORD);
-            event.accept(ItemsInit.LEAD_CHESTPLATE);
-            event.accept(ItemsInit.LEAD_HELMET);
-            event.accept(ItemsInit.LEAD_LEGGINGS);
-            event.accept(ItemsInit.LEAD_BOOTS);
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_SWORD.getDefaultInstance(),
+                            ItemsInit.LEAD_SWORD.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_BOOTS.getDefaultInstance(),
+                            ItemsInit.LEAD_BOOTS.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_BOOTS.getDefaultInstance(),
+                            ItemsInit.LEAD_LEGGINGS.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_BOOTS.getDefaultInstance(),
+                            ItemsInit.LEAD_HELMET.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_BOOTS.getDefaultInstance(),
+                            ItemsInit.LEAD_CHESTPLATE.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
         }
         if(event.getTabKey().equals(CreativeModeTabs.TOOLS_AND_UTILITIES)) {
-            event.accept(ItemsInit.LEAD_AXE);
-            event.accept(ItemsInit.LEAD_HOE);
-            event.accept(ItemsInit.LEAD_PICKAXE);
-            event.accept(ItemsInit.LEAD_SHOVEL);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_HOE.getDefaultInstance(),
+                            ItemsInit.LEAD_HOE.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_HOE.getDefaultInstance(),
+                            ItemsInit.LEAD_AXE.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_HOE.getDefaultInstance(),
+                            ItemsInit.LEAD_PICKAXE.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+
+            event.getEntries()
+                    .putAfter(
+                            Items.NETHERITE_HOE.getDefaultInstance(),
+                            ItemsInit.LEAD_SHOVEL.get().getDefaultInstance(),
+                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
         }
+
         if(event.getTabKey().equals(CreativeModeTabs.INGREDIENTS)) {
             event.accept(ItemsInit.YELLOWCAKE);
         }
