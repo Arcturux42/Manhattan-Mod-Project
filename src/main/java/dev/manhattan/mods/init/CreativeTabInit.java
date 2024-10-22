8985d1e35f45257430e6ac844d44cdda57e5973a
Arcturux <arcturux42@gmail.com>
2024-10-22 13:46:59 +0200

    Correction of creative tab

--- src/main/java/dev/manhattan/mods/init/CreativeTabInit.java
+++ src/main/java/dev/manhattan/mods/init/CreativeTabInit.java
@@ -3,19 +3,16 @@ package dev.manhattan.mods.init;
 import dev.manhattan.mods.Mods;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +21,6 @@ import java.util.function.Supplier;
 @Mod.EventBusSubscriber(modid = Mods.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class CreativeTabInit {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Mods.MODID);
-
     public static final List<Supplier<? extends ItemLike>> MANHATTAN_TAB_ITEMS = new ArrayList<>();
 
     public static final RegistryObject<CreativeModeTab> MANHATTAN_TAB = TABS.register("manhattan_tab",
@@ -38,107 +34,105 @@ public class CreativeTabInit {
                     .build()
     );
 
-    public static <T extends Item> RegistryObject<T> addToTab(RegistryObject<T> itemLike) {
-        MANHATTAN_TAB_ITEMS.add(itemLike);
-        return itemLike;
+    private static void addToTabs(BuildCreativeModeTabContentsEvent event, ResourceKey<CreativeModeTab> tab, ItemLike afterItem, Supplier<? extends Item> item, boolean placeAfter) {
+        if (event.getTabKey().equals(tab)) {
+            ItemStack afterItemStack = afterItem != null ? new ItemStack(afterItem.asItem()) : ItemStack.EMPTY; // Gérer le cas où afterItem est null
+            ItemStack itemStack = item.get().getDefaultInstance();
+
+            if (placeAfter && afterItem != null) {
+                // Ajoute l'item après afterItem seulement si afterItem n'est pas null
+                event.getEntries().putAfter(afterItemStack, itemStack, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            } else {
+                // Ajoute l'item sans se soucier de l'ordre
+                event.getEntries().put(itemStack, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            }
+        }
     }
+
 
     @SubscribeEvent
     public static void buildContents(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey().equals(CreativeModeTabs.BUILDING_BLOCKS)) {
-            event.getEntries().putAfter(Items.BASALT.getDefaultInstance(), ItemsInit.URANIUM_BLOCK_ITEM.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            event.accept(ItemsInit.CARBON_BLOCK_ITEM);
-            event.accept(ItemsInit.LEAD_BLOCK_ITEM);
+        // MANHATTAN
+        if (event.getTabKey().equals(MANHATTAN_TAB.getKey())) {
+            // BUILDING_BLOCKS
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.URANIUM_BLOCK_ITEM, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.CARBON_BLOCK_ITEM, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_BLOCK_ITEM, false);
+
+            // FOOD_AND_DRINKS
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.CHEESE, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.CHOCOLATE, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.SLICE_OF_PIZZA, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.PIZZA, false);
+
+            // Ingredients
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_INGOT, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.URANIUM_RAW, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.URANIUM_INGOT, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.YELLOWCAKE, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.FLOUR, false);
+
+            // NATURAL_BLOCKS
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.URANIUM_ORE_ITEM, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.DEEPSLATE_URANIUM_ORE_ITEM, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_ORE_ITEM, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.DEEPSLATE_LEAD_ORE_ITEM, false);
+
+            // COMBAT
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_SWORD, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_BOOTS, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_LEGGINGS, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_HELMET, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_CHESTPLATE, false);
+
+            // TOOLS_AND_UTILITIES
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_HOE, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_AXE, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_PICKAXE, false);
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.LEAD_SHOVEL, false);
+
+            // Functional block
+            addToTabs(event, MANHATTAN_TAB.getKey(), null, ItemsInit.CHEMICAL_LEACHING_MACHINE_ITEM, false);
         }
 
-        if(event.getTabKey().equals(CreativeModeTabs.FOOD_AND_DRINKS)) {
+        // BUILDING_BLOCKS
+        addToTabs(event, CreativeModeTabs.BUILDING_BLOCKS, Items.BASALT, ItemsInit.URANIUM_BLOCK_ITEM, true);
+        addToTabs(event, CreativeModeTabs.BUILDING_BLOCKS, Items.BASALT, ItemsInit.CARBON_BLOCK_ITEM, true);
+        addToTabs(event, CreativeModeTabs.BUILDING_BLOCKS, Items.BASALT, ItemsInit.LEAD_BLOCK_ITEM, true);
 
-            event.getEntries()
-                    .putAfter(
-                            Items.PUMPKIN_PIE.getDefaultInstance(),
-                            ItemsInit.CHEESE.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        // FOOD_AND_DRINKS
+        addToTabs(event, CreativeModeTabs.FOOD_AND_DRINKS, Items.PUMPKIN_PIE, ItemsInit.CHEESE, true);
+        addToTabs(event, CreativeModeTabs.FOOD_AND_DRINKS, Items.PUMPKIN_PIE, ItemsInit.CHOCOLATE, true);
+        addToTabs(event, CreativeModeTabs.FOOD_AND_DRINKS, Items.PUMPKIN_PIE, ItemsInit.SLICE_OF_PIZZA, true);
+        addToTabs(event, CreativeModeTabs.FOOD_AND_DRINKS, Items.PUMPKIN_PIE, ItemsInit.PIZZA, true);
 
-            event.getEntries()
-                    .putAfter(
-                            Items.PUMPKIN_PIE.getDefaultInstance(),
-                            ItemsInit.CHOCOLATE.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        // NATURAL_BLOCKS
+        addToTabs(event, CreativeModeTabs.NATURAL_BLOCKS, Items.NETHER_QUARTZ_ORE, ItemsInit.URANIUM_ORE_ITEM, true);
+        addToTabs(event, CreativeModeTabs.NATURAL_BLOCKS, Items.NETHER_QUARTZ_ORE, ItemsInit.DEEPSLATE_URANIUM_ORE_ITEM, true);
+        addToTabs(event, CreativeModeTabs.NATURAL_BLOCKS, Items.NETHER_QUARTZ_ORE, ItemsInit.LEAD_ORE_ITEM, true);
+        addToTabs(event, CreativeModeTabs.NATURAL_BLOCKS, Items.NETHER_QUARTZ_ORE, ItemsInit.DEEPSLATE_LEAD_ORE_ITEM, true);
 
-            event.getEntries()
-                    .putAfter(
-                            Items.PUMPKIN_PIE.getDefaultInstance(),
-                            ItemsInit.SLICE_OF_PIZZA.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        // COMBAT
+        addToTabs(event, CreativeModeTabs.COMBAT, Items.NETHERITE_SWORD, ItemsInit.LEAD_SWORD, true);
+        addToTabs(event, CreativeModeTabs.COMBAT, Items.NETHERITE_BOOTS, ItemsInit.LEAD_BOOTS, true);
+        addToTabs(event, CreativeModeTabs.COMBAT, Items.NETHERITE_BOOTS, ItemsInit.LEAD_LEGGINGS, true);
+        addToTabs(event, CreativeModeTabs.COMBAT, Items.NETHERITE_BOOTS, ItemsInit.LEAD_HELMET, true);
+        addToTabs(event, CreativeModeTabs.COMBAT, Items.NETHERITE_BOOTS, ItemsInit.LEAD_CHESTPLATE, true);
 
-            event.getEntries()
-                    .putAfter(
-                            Items.PUMPKIN_PIE.getDefaultInstance(),
-                            ItemsInit.PIZZA.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-        }
+        // TOOLS_AND_UTILITIES
+        addToTabs(event, CreativeModeTabs.TOOLS_AND_UTILITIES, Items.NETHERITE_HOE, ItemsInit.LEAD_HOE, true);
+        addToTabs(event, CreativeModeTabs.TOOLS_AND_UTILITIES, Items.NETHERITE_HOE, ItemsInit.LEAD_AXE, true);
+        addToTabs(event, CreativeModeTabs.TOOLS_AND_UTILITIES, Items.NETHERITE_HOE, ItemsInit.LEAD_PICKAXE, true);
+        addToTabs(event, CreativeModeTabs.TOOLS_AND_UTILITIES, Items.NETHERITE_HOE, ItemsInit.LEAD_SHOVEL, true);
 
-        if(event.getTabKey().equals(CreativeModeTabs.NATURAL_BLOCKS)) {
-            event.accept(ItemsInit.LEAD_ORE_ITEM);
-            event.accept(ItemsInit.URANIUM_ORE_ITEM);
-        }
-        if(event.getTabKey().equals(CreativeModeTabs.COMBAT)) {
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_SWORD.getDefaultInstance(),
-                            ItemsInit.LEAD_SWORD.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        // INGREDIENTS
+        addToTabs(event, CreativeModeTabs.INGREDIENTS, Items.GUNPOWDER, ItemsInit.YELLOWCAKE, true);
+        addToTabs(event, CreativeModeTabs.INGREDIENTS, Items.NETHERITE_INGOT, ItemsInit.LEAD_INGOT, true);
+        addToTabs(event, CreativeModeTabs.INGREDIENTS, Items.DIAMOND, ItemsInit.URANIUM_RAW, true);
+        addToTabs(event, CreativeModeTabs.INGREDIENTS, Items.NETHERITE_INGOT, ItemsInit.URANIUM_INGOT, true);
+        addToTabs(event, CreativeModeTabs.INGREDIENTS, Items.WHEAT, ItemsInit.FLOUR, true);
 
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_BOOTS.getDefaultInstance(),
-                            ItemsInit.LEAD_BOOTS.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_BOOTS.getDefaultInstance(),
-                            ItemsInit.LEAD_LEGGINGS.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_BOOTS.getDefaultInstance(),
-                            ItemsInit.LEAD_HELMET.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_BOOTS.getDefaultInstance(),
-                            ItemsInit.LEAD_CHESTPLATE.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-        }
-        if(event.getTabKey().equals(CreativeModeTabs.TOOLS_AND_UTILITIES)) {
-
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_HOE.getDefaultInstance(),
-                            ItemsInit.LEAD_HOE.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_HOE.getDefaultInstance(),
-                            ItemsInit.LEAD_AXE.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_HOE.getDefaultInstance(),
-                            ItemsInit.LEAD_PICKAXE.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-
-            event.getEntries()
-                    .putAfter(
-                            Items.NETHERITE_HOE.getDefaultInstance(),
-                            ItemsInit.LEAD_SHOVEL.get().getDefaultInstance(),
-                            CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-        }
-
-        if(event.getTabKey().equals(CreativeModeTabs.INGREDIENTS)) {
-            event.accept(ItemsInit.YELLOWCAKE);
-        }
+        // Functional block
+        addToTabs(event, CreativeModeTabs.FUNCTIONAL_BLOCKS, Items.BREWING_STAND, ItemsInit.CHEMICAL_LEACHING_MACHINE_ITEM, true);
     }
 }
--- src/main/java/dev/manhattan/mods/init/ItemsInit.java
+++ src/main/java/dev/manhattan/mods/init/ItemsInit.java
@@ -7,8 +7,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import static dev.manhattan.mods.init.CreativeTabInit.addToTab;
-
 public class ItemsInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Mods.MODID);
 
@@ -21,6 +19,7 @@ public class ItemsInit {
                             .build())
                     .rarity(Rarity.COMMON)
             ));
+
     public static final RegistryObject<Item> CHEESE = ITEMS.register("cheese",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
@@ -30,6 +29,7 @@ public class ItemsInit {
                             .build())
                     .rarity(Rarity.COMMON)
             ));
+
     public static final RegistryObject<Item> PIZZA = ITEMS.register("pizza",
             () -> new Item(new Item.Properties()
                     .stacksTo(1)
@@ -39,6 +39,7 @@ public class ItemsInit {
                             .build())
                     .rarity(Rarity.UNCOMMON)
             ));
+
     public static final RegistryObject<Item> SLICE_OF_PIZZA = ITEMS.register("slice_of_pizza",
             () -> new Item(new Item.Properties()
                     .stacksTo(16)
@@ -48,146 +49,150 @@ public class ItemsInit {
                             .build())
                     .rarity(Rarity.COMMON)
             ));
+
     public static final RegistryObject<Item> FLOUR = ITEMS.register("flour",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
                     .rarity(Rarity.COMMON)
             ));
 
-    public static final RegistryObject<BlockItem> URANIUM_BLOCK_ITEM = addToTab(ITEMS.register("uranium_block",
+    public static final RegistryObject<BlockItem> URANIUM_BLOCK_ITEM = ITEMS.register("uranium_block",
             () -> new BlockItem(BlocksInit.URANIUM_BLOCK.get(),
                     new Item.Properties()
                             .rarity(Rarity.COMMON)
-            )));
+            ));
 
-    public static final RegistryObject<BlockItem> LEAD_BLOCK_ITEM = addToTab(ITEMS.register("lead_block",
+    public static final RegistryObject<BlockItem> LEAD_BLOCK_ITEM = ITEMS.register("lead_block",
             () -> new BlockItem(BlocksInit.LEAD_BLOCK.get(),
                     new Item.Properties()
                             .rarity(Rarity.COMMON)
-            )));
+            ));
 
-    public static final RegistryObject<BlockItem> CARBON_BLOCK_ITEM = addToTab(ITEMS.register("carbon_block",
+    public static final RegistryObject<BlockItem> CARBON_BLOCK_ITEM = ITEMS.register("carbon_block",
             () -> new BlockItem(BlocksInit.CARBON_BLOCK.get(),
                     new Item.Properties()
                             .rarity(Rarity.COMMON)
-            )));
+            ));
 
-    public static final RegistryObject<BlockItem> LEAD_ORE_ITEM = addToTab(ITEMS.register("lead_ore",
+    public static final RegistryObject<BlockItem> LEAD_ORE_ITEM = ITEMS.register("lead_ore",
             () -> new BlockItem(BlocksInit.LEAD_ORE.get(),
                     new Item.Properties()
                             .rarity(Rarity.COMMON)
-            )));
+            ));
 
-    public static final RegistryObject<BlockItem> DEEPSLATE_LEAD_ORE_ITEM = addToTab(ITEMS.register("deepslate_lead_ore",
+    public static final RegistryObject<BlockItem> DEEPSLATE_LEAD_ORE_ITEM = ITEMS.register("deepslate_lead_ore",
             () -> new BlockItem(BlocksInit.DEEPSLATE_LEAD_ORE.get(),
                     new Item.Properties()
                             .rarity(Rarity.COMMON)
-            )));
+            ));
+
     public static final RegistryObject<Item> LEAD_INGOT = ITEMS.register("lead_ingot",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
                     .rarity(Rarity.COMMON)
             ));
 
-    public static final RegistryObject<SwordItem> LEAD_SWORD = addToTab(ITEMS.register("lead_sword",
+    public static final RegistryObject<SwordItem> LEAD_SWORD = ITEMS.register("lead_sword",
             () -> new SwordItem(
                     TiersInit.LEAD,
                     3,
                     -0.6f,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<PickaxeItem> LEAD_PICKAXE = addToTab(ITEMS.register("lead_pickaxe",
+    public static final RegistryObject<PickaxeItem> LEAD_PICKAXE = ITEMS.register("lead_pickaxe",
             () -> new PickaxeItem(
                     TiersInit.LEAD,
                     2,
                     1.2f,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<AxeItem> LEAD_AXE = addToTab(ITEMS.register("lead_axe",
+    public static final RegistryObject<AxeItem> LEAD_AXE = ITEMS.register("lead_axe",
             () -> new AxeItem(
                     TiersInit.LEAD,
                     2,
                     1.2f,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<ShovelItem> LEAD_SHOVEL = addToTab(ITEMS.register("lead_shovel",
+    public static final RegistryObject<ShovelItem> LEAD_SHOVEL = ITEMS.register("lead_shovel",
             () -> new ShovelItem(
                     TiersInit.LEAD,
                     1,
                     1.5f,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<HoeItem> LEAD_HOE = addToTab(ITEMS.register("lead_hoe",
+    public static final RegistryObject<HoeItem> LEAD_HOE = ITEMS.register("lead_hoe",
             () -> new HoeItem(
                     TiersInit.LEAD,
                     1,
                     1.5f,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<ArmorItem> LEAD_HELMET = addToTab(ITEMS.register("lead_helmet",
+    public static final RegistryObject<ArmorItem> LEAD_HELMET = ITEMS.register("lead_helmet",
             () -> new ArmorItem(
                     ArmorMaterialInit.LEAD,
                     ArmorItem.Type.HELMET,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<ArmorItem> LEAD_CHESTPLATE = addToTab(ITEMS.register("lead_chestplate",
+    public static final RegistryObject<ArmorItem> LEAD_CHESTPLATE = ITEMS.register("lead_chestplate",
             () -> new ArmorItem(
                     ArmorMaterialInit.LEAD,
                     ArmorItem.Type.CHESTPLATE,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<ArmorItem> LEAD_LEGGINGS = addToTab(ITEMS.register("lead_leggings",
+    public static final RegistryObject<ArmorItem> LEAD_LEGGINGS = ITEMS.register("lead_leggings",
             () -> new ArmorItem(
                     ArmorMaterialInit.LEAD,
                     ArmorItem.Type.LEGGINGS,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<ArmorItem> LEAD_BOOTS = addToTab(ITEMS.register("lead_boots",
+    public static final RegistryObject<ArmorItem> LEAD_BOOTS = ITEMS.register("lead_boots",
             () -> new ArmorItem(
                     ArmorMaterialInit.LEAD,
                     ArmorItem.Type.BOOTS,
                     new Item.Properties()
-            )));
+            ));
 
-    public static final RegistryObject<BlockItem> URANIUM_ORE_ITEM = addToTab(ITEMS.register("uranium_ore",
+    public static final RegistryObject<BlockItem> URANIUM_ORE_ITEM = ITEMS.register("uranium_ore",
             () -> new BlockItem(BlocksInit.URANIUM_ORE.get(),
                     new Item.Properties()
                             .rarity(Rarity.COMMON)
-            )));
-    public static final RegistryObject<BlockItem> DEEPSLATE_URANIUM_ORE_ITEM = addToTab(ITEMS.register("deepslate_uranium_ore",
+            ));
+
+    public static final RegistryObject<BlockItem> DEEPSLATE_URANIUM_ORE_ITEM = ITEMS.register("deepslate_uranium_ore",
             () -> new BlockItem(BlocksInit.DEEPSLATE_URANIUM_ORE.get(),
                     new Item.Properties()
                             .rarity(Rarity.COMMON)
-            )));
+            ));
 
-    public static final RegistryObject<Item> URANIUM_RAW = addToTab(ITEMS.register("uranium_raw",
+    public static final RegistryObject<Item> URANIUM_RAW = ITEMS.register("uranium_raw",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
                     .rarity(Rarity.COMMON)
-            )));
-    public static final RegistryObject<Item> URANIUM_INGOT = addToTab(ITEMS.register("uranium_ingot",
+            ));
+
+    public static final RegistryObject<Item> URANIUM_INGOT = ITEMS.register("uranium_ingot",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
                     .rarity(Rarity.COMMON)
-            )));
+            ));
 
-    public static final RegistryObject<Item> YELLOWCAKE = addToTab(ITEMS.register("yellowcake",
+    public static final RegistryObject<Item> YELLOWCAKE = ITEMS.register("yellowcake",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
                     .rarity(Rarity.COMMON)
-            )));
+            ));
 
-    public static final RegistryObject<BlockItem> CHEMICAL_LEACHING_MACHINE_ITEM = addToTab(ITEMS.register("chemical_leaching_machine_item",
+    public static final RegistryObject<BlockItem> CHEMICAL_LEACHING_MACHINE_ITEM = ITEMS.register("chemical_leaching_machine_item",
             () -> new BlockItem(BlocksInit.CHEMICAL_LEACHING_MACHINE_BLOCK.get(),
                     new Item.Properties()
                             .rarity(Rarity.COMMON)
-            )));
+            ));
 }
